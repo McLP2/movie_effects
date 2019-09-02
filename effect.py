eedbc38c17c76e612cfbc782e44d8f9b98298b3e
McLP <wdv50jkc@gmail.com>
2019-09-02 17:09:40 +0200

    added more options

--- effect.py
+++ effect.py
@@ -5,6 +5,8 @@ class Effect(ABC):
     def __init__(self, params, movie):
         self.params = params
         self.movie = movie
+        self.has_frames = False
+        self.allow_multiprocessing = True
 
     @abstractmethod
     def apply(self, frame, frame_number):
